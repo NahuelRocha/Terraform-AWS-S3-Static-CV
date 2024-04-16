20298cab43c13896e23687319f4f41fca6497764
Rocha Nahuel <rocha.nahuel@hotmail.com>
2024-04-16 02:02:58 -0300

    added cv project v1

---
+++ variable.tf
@@ -0,0 +1,12 @@
+# Define variables for reusability and easy configuration
+variable "aws_region" {
+  default = "ca-central-1"
+}
+
+variable "bucket_name" {
+  default = "my-static-website-bucket-67543"
+}
+
+variable "website_index_document" {
+  default = "index.html"
+}
